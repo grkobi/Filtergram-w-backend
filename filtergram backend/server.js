1fb804aad9c156c7a2bc9f42be66aeb0001a6083
grkobi <gr.kobi@gmail.com>
2023-06-27 16:56:41 +0300

    updated build

--- filtergram backend/server.js
+++ filtergram backend/server.js
@@ -6,6 +6,16 @@ const cookieParser = require('cookie-parser')
 const path = require('path')
 const cors = require('cors')
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname,'public')))
+   } else {
+    const corsOptions = {
+    origin: ['http://127.0.0.1:3000', 'http://localhost:3000', 'http://127.0.0.1:8080','http://localhost:8080','http://localhost:3001'],
+    credentials: true
+    }
+    app.use(cors(corsOptions))
+   }
+
 // App Configuration
 const corsOptions = {
     origin: [
@@ -138,10 +148,17 @@ app.post('/api/auth/logout', (req, res) => {
 })
 
 
-app.get('/**', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+// app.get('/**', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
+// })
 
 
 // Listen will always be the last line in our server!
-app.listen(3030, () => console.log('Server listening on port 3030!'))
+// app.listen(3030, () => console.log('Server listening on port 3030!'))
+const port = process.env.PORT || 3030;
+app.get('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+app.listen(port, () => {
+ console.log(`App listening on port ${port}!`)
+});
